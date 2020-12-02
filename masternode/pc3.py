e3a99f700bfa711c6601d8630c416086808fcc99
ARK22 <59229741+Dhoni-myk@users.noreply.github.com>
2020-12-02 20:23:02 +0700

    Add files via upload

---
+++ masternode/pc3.py
@@ -0,0 +1,16 @@
+import paramiko
+import time
+
+user_name ="root"
+passwd="dhonitt123"
+ip="192.168.43.98"
+ssh_client=paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=ip,username=user_name,password=passwd, look_for_keys=False)
+conn=ssh_client.get_transport().open_session()
+conn.invoke_shell()
+conn.send("scl enable rh-python36 bash\n")
+conn.send("python3 tugas.py\n")
+output =conn.recv(65535)
+print(output.decode("ascii"))
+print("      ")
